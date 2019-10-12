c4dbf1f2cf034ca713b47970c51240398107fa7a
Christian Benjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-10-12 17:16:22 +0200

    Updated version

--- ReadingUserInput.java
+++ ReadingUserInput.java
@@ -5,15 +5,27 @@ import java.util.Scanner;
 public class ReadingUserInput {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.print("Enter your year of birth ");
-        int yearOfBirth = scan.nextInt();
-        scan.nextLine();
 
-        System.out.print("Enter your name: ");
-        String name = scan.nextLine();
-        int age = 2019 - yearOfBirth;
+        boolean hasNextInt = scan.hasNextInt();
 
-        System.out.print("\nYour name is " + name + ", and you are " + age + " years old");
+        if(hasNextInt) {
+            int yearOfBirth = scan.nextInt();
+            scan.nextLine();
+
+            System.out.print("Enter your name: ");
+            String name = scan.nextLine();
+            int age = 2019 - yearOfBirth;
+
+            if(age >= 0 && age <= 100 ){
+                System.out.print("Your name is " + name + ", and you are " + age + " years old");
+            } else {
+                System.out.print("Invalid year of birth");
+            }
+        } else {
+            System.out.print("Unable to parse year of birth");
+        }
 
         scan.close();
     }
